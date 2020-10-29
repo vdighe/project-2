540c6f9308328065d1e03095644230d6989a0499
Vaishali Dighe-Phanse <vaishphanse@gmail.com>
2020-10-29 12:23:45 -0400

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
-const db = mongoose.connection;v
+const db = mongoose.connection;
 //___________________
 //Port
 //___________________
@@ -17,7 +17,12 @@ const PORT = process.env.PORT || 3000;
 // How to connect to the database either via heroku or locally
 const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/'+ 'vdighe-project2';
 // Connect to Mongo
-mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
+mongoose.connect(MONGODB_URI ,    {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+},);
 // Error / success
 db.on('error', (err) => console.log(err.message + ' is Mongod not running?'));
 db.on('connected', () => console.log('mongo connected: ', MONGODB_URI));
