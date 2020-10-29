bbc4415d2a8d673525ada8f1b4575bda149246c2
Vaishali Dighe-Phanse <vaishphanse@gmail.com>
2020-10-29 12:16:15 -0400

    Second Commit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
-const db = mongoose.connection;
+const db = mongoose.connection;v
 //___________________
 //Port
 //___________________
